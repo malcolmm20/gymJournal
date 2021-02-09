42b6f9c58df4870ba1fb185f0e9cb7f251aa9b24
malcolm <malcolmmackenzie20@gmail.com>
2021-02-09 15:46:21 -0800

    implemented RoutineTest class`

--- src/test/model/OpenWorkoutExerciseSetTest.java
+++ src/test/model/OpenWorkoutExerciseSetTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class OpenWorkoutWorkoutExerciseSetTest {
+public class OpenWorkoutExerciseSetTest {
     private WorkoutExerciseSet setA;
     private WorkoutExerciseSet setB;
 
--- src/test/model/RoutineTest.java
+++ src/test/model/RoutineTest.java
@@ -3,7 +3,88 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RoutineTest {
+    private ArrayList<RoutineExercise> exerciseArrayListA;
+    private ArrayList<RoutineExercise> exerciseArrayListB;
+    private ArrayList<RoutineExercise> exerciseArrayListC;
+    private RoutineExercise exerciseA;
+    private RoutineExercise exerciseB;
+    private RoutineExercise exerciseC;
+    private RoutineExercise exerciseD;
+    private RoutineExercise exerciseE;
+    private RoutineExercise exerciseF;
+    private Routine routineA;
+    private Routine routineB;
+    private Routine routineC;
+    private Routine routineD;
+
+
+    @BeforeEach
+    void setUp() {
+        exerciseArrayListA = new ArrayList<>();
+        exerciseArrayListB = new ArrayList<>();
+        exerciseArrayListC = new ArrayList<>();
+        routineA = new Routine("chest day");
+        routineB = new Routine("chest day");
+        routineC = new Routine("leg day");
+        exerciseA = new RoutineExercise("bench press", 4);
+        exerciseB = new RoutineExercise("incline bench press", 4);
+        exerciseC = new RoutineExercise("squats", 5);
+        exerciseD = new RoutineExercise("leg press", 4);
+        routineA.addExercise(exerciseA);
+        routineA.addExercise(exerciseB);
+        routineB.addExercise(exerciseA);
+        routineB.addExercise(exerciseB);
+        routineC.addExercise(exerciseC);
+        routineC.addExercise(exerciseD);
+    }
+
+    @Test
+    void testEquals() {
+        assertTrue(routineA.equals(routineA));
+        assertTrue(routineA.equals(routineB) && routineB.equals(routineA));
+        assertFalse(routineA.equals(routineD));
+        assertFalse(routineA.equals(routineC) || routineC.equals(routineA));
+
+        routineB.addExercise(exerciseA);
+        assertFalse(routineA.equals(routineB) || routineB.equals(routineA));
+    }
+
+    @Test
+    void testHashCode() {
+        assertTrue(routineA.hashCode() == routineB.hashCode());
+        assertFalse(routineA.hashCode() == routineC.hashCode());
+    }
+
+    @Test
+    void testGetName() {
+        assertEquals("chest day", routineA.getName());
+        assertEquals("leg day", routineC.getName());
+    }
+
+    @Test
+    void testGetExercises() {
+        exerciseArrayListA.add(exerciseA);
+        exerciseArrayListA.add(exerciseB);
+        exerciseArrayListB.add(exerciseC);
+        exerciseArrayListB.add(exerciseD);
+        exerciseArrayListC.add(exerciseD);
+        exerciseArrayListC.add(exerciseC);
+
+        assertEquals(exerciseArrayListA, routineA.getExercises());
+        assertEquals(exerciseArrayListB, routineC.getExercises());
+        assertNotEquals(exerciseArrayListC, routineC.getExercises());
+    }
+
+    @Test
+    void testToString() {
+        assertEquals(routineA.getName().concat("\n" + exerciseA.toString()
+                + "\n" + exerciseB.toString() + "\n"), routineA.toString());
+        assertEquals(routineC.getName().concat("\n" + exerciseC.toString()
+                + "\n" + exerciseD.toString() + "\n"), routineC.toString());
+    }
 }
